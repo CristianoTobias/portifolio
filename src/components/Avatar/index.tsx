fe3f2a644b66bc9fa376a3f3e0b37ea2b7d77313
Cristiano Tobias <cristiano.tobias40@gmail.com>
2023-05-29 21:04:57 -0300

    Estilzando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,6 @@
+import { FotoPerfil } from './style'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/cristianotobias.png" alt="" />
+)
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,13 +1,22 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './style'
 
 const Sidebar = () => {
   return (
     <aside>
-      <img
-        src="https://github.com/cristianotobias.png"
-        alt="This is Cristiano tobias"
-      />
-      <Titulo fontSize={20}>Cristiano Tobias</Titulo>
+      <SidebarContainer>
+        <Avatar />
+        <Titulo fontSize={20}>Cristiano Tobias</Titulo>
+        <Paragrafo tipo="secundario" fontSize={16}>
+          cristianotobias
+        </Paragrafo>
+        <Descricao tipo="principal" fontSize={12}>
+          Bacharelado Engenharia de Software
+        </Descricao>
+        <BotaoTema>Trocar Tema</BotaoTema>
+      </SidebarContainer>
     </aside>
   )
 }
